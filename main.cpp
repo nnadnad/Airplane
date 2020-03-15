904eb4fac33ec5d8b6c0511b8566051b9a0aff6d
Valentino Simamora <43459761+nnadnad@users.noreply.github.com>
2020-03-15 16:18:03 +0700

    add glulookat

--- main.cpp
+++ main.cpp
@@ -44,7 +44,7 @@ static void display(void)
     glTranslatef(0.0f, 0.0f, -10.5f);
 
     gluLookAt(posX, posY, posZ,
-              viewX + cos(rotateXZ * M_PI / 180), viewY, viewZ + sin(rotateXZ * M_PI / 180),
+             viewX + cos(rotateXZ * M_PI / 180), viewY, viewZ + sin(rotateXZ * M_PI / 180),
               upX + cos(rotateXY * M_PI / 180), upY + sin(rotateXY * M_PI / 180), upZ);
 
     glScalef(1.0 + zoom, 1.0 + zoom, 1.0 + zoom);
