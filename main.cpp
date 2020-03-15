62156f9eb41d82d04e859367aa847d6adf8b71f0
Valentino Simamora <43459761+nnadnad@users.noreply.github.com>
2020-03-15 08:36:27 +0700

    add sayap and change window baackground

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ static float upZ = 0.0;
 
 static void init(void)
 {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
     glShadeModel(GL_FLAT);
 }
 
@@ -38,37 +38,85 @@ static void display(void)
 
     glScalef(1.0 + zoom, 1.0 + zoom, 1.0 + zoom);
 
-    glBegin(GL_QUADS);        // Draw The Cube Using quads
-    glColor3f(0.0f,1.0f,0.0f);    // Color Blue
-    glVertex3f( 1.0f, 1.0f,-1.0f);    // Top Right Of The Quad (Top)
-    glVertex3f(-1.0f, 1.0f,-1.0f);    // Top Left Of The Quad (Top)
-    glVertex3f(-1.0f, 1.0f, 1.0f);    // Bottom Left Of The Quad (Top)
-    glVertex3f( 1.0f, 1.0f, 1.0f);    // Bottom Right Of The Quad (Top)
-    glColor3f(1.0f,0.5f,0.0f);    // Color Orange
-    glVertex3f( 1.0f,-1.0f, 1.0f);    // Top Right Of The Quad (Bottom)
-    glVertex3f(-1.0f,-1.0f, 1.0f);    // Top Left Of The Quad (Bottom)
-    glVertex3f(-1.0f,-1.0f,-1.0f);    // Bottom Left Of The Quad (Bottom)
-    glVertex3f( 1.0f,-1.0f,-1.0f);    // Bottom Right Of The Quad (Bottom)
-    glColor3f(1.0f,0.0f,0.0f);    // Color Red
-    glVertex3f( 1.0f, 1.0f, 1.0f);    // Top Right Of The Quad (Front)
-    glVertex3f(-1.0f, 1.0f, 1.0f);    // Top Left Of The Quad (Front)
-    glVertex3f(-1.0f,-1.0f, 1.0f);    // Bottom Left Of The Quad (Front)
-    glVertex3f( 1.0f,-1.0f, 1.0f);    // Bottom Right Of The Quad (Front)
-    glColor3f(1.0f,1.0f,0.0f);    // Color Yellow
-    glVertex3f( 1.0f,-1.0f,-1.0f);    // Top Right Of The Quad (Back)
-    glVertex3f(-1.0f,-1.0f,-1.0f);    // Top Left Of The Quad (Back)
-    glVertex3f(-1.0f, 1.0f,-1.0f);    // Bottom Left Of The Quad (Back)
-    glVertex3f( 1.0f, 1.0f,-1.0f);    // Bottom Right Of The Quad (Back)
+
+    /*sayap bawah*/
+
+    glBegin(GL_POLYGON);
     glColor3f(0.0f,0.0f,1.0f);    // Color Blue
-    glVertex3f(-1.0f, 1.0f, 1.0f);    // Top Right Of The Quad (Left)
-    glVertex3f(-1.0f, 1.0f,-1.0f);    // Top Left Of The Quad (Left)
-    glVertex3f(-1.0f,-1.0f,-1.0f);    // Bottom Left Of The Quad (Left)
-    glVertex3f(-1.0f,-1.0f, 1.0f);    // Bottom Right Of The Quad (Left)
-    glColor3f(1.0f,0.0f,1.0f);    // Color Violet
-    glVertex3f( 1.0f, 1.0f,-1.0f);    // Top Right Of The Quad (Right)
-    glVertex3f( 1.0f, 1.0f, 1.0f);    // Top Left Of The Quad (Right)
-    glVertex3f( 1.0f,-1.0f, 1.0f);    // Bottom Left Of The Quad (Right)
-    glVertex3f( 1.0f,-1.0f,-1.0f);    // Bottom Right Of The Quad (Right)
+    glVertex3f( 0.6, -0.3, 0);
+    glVertex3f( 0.3, -0.31, 0);
+    glVertex3f( 0.3, -0.31, 1);
+    glVertex3f( 0.6, -0.3, 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+    glVertex3f( 0.6, -0.27, 0);
+    glVertex3f( 0.3, -0.29, 0);
+    glVertex3f( 0.3, -0.31, 1);
+    glVertex3f( 0.6, -0.3, 1);
+
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+    glVertex3f( 0.6, -0.3,-0.2);
+    glVertex3f( 0.3, -0.31, -0.2);
+    glVertex3f( 0.3, -0.31, -1.2);
+    glVertex3f( 0.6, -0.3, -1.2);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+    glVertex3f( 0.6, -0.27, -0.2);
+    glVertex3f( 0.3, -0.29, -0.2);
+    glVertex3f( 0.3, -0.31, -1.2);
+    glVertex3f( 0.6, -0.3, -1.2);
+
+    glEnd();
+
+    /*sayap atas*/
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+    glVertex3f( 0.6, 0.3, 0);
+    glVertex3f( 0.3, 0.29, 0);
+    glVertex3f( 0.3, 0.29, 1);
+    glVertex3f( 0.6, 0.3, 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+    glVertex3f( 0.6, 0.33, 0);
+    glVertex3f( 0.3, 0.29, 0);
+    glVertex3f( 0.3, 0.29, 1);
+    glVertex3f( 0.6, 0.30, 1);
+
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+    glVertex3f( 0.6, 0.3,-0.2);
+    glVertex3f( 0.3, 0.29, -0.2);
+    glVertex3f( 0.3, 0.29, -1.2);
+    glVertex3f( 0.6, 0.3, -1.2);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+    glVertex3f( 0.6, 0.33, -0.2);
+    glVertex3f( 0.3, 0.29, -0.2);
+    glVertex3f( 0.3, 0.31, -1.2);
+    glVertex3f( 0.6, 0.3, -1.2);
+
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+    glVertex3f( 0.6, 0.33, -0.2);
+    glVertex3f( 0.3, 0.29, -0.2);
+    glVertex3f( 0.3, 0.29, 0);
+    glVertex3f( 0.6, 0.33, 0);
 
     glEnd();
 
