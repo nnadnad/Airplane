1d9f3b97df78abf9c6d79a7f80edd4c9456d3bb1
Hans <yanghansen02@gmail.com>
2020-03-16 19:54:26 +0700

    minor model fix

--- model.cpp
+++ model.cpp
@@ -20,7 +20,7 @@ float angle = 0.0;
 
 static void display(void)
 {
-    glClear(GL_glColor3f_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glTranslatef(0.0f, 0.0f, -10.5f);
