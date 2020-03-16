e084765f5a8dad1c053df706241ec7c90b6826bb
vincentbudianto <vincent.budianto@gmail.com>
2020-03-16 21:41:30 +0700

    add help option

--- main.cpp
+++ main.cpp
@@ -17,15 +17,46 @@
 #include "model.cpp"
 #include "camera.cpp"
 
+void printHelp()
+{
+    std::cout << "      Airplane Model with GLUT OpenGL      " << std::endl
+              << "___________________________________________" << std::endl
+              << "|       |                                 |" << std::endl
+              << "|   q   |   exit                          |" << std::endl
+              << "|   r   |   reset default view            |" << std::endl
+              << "|   w   |   rotate model right (Z-axis)   |" << std::endl
+              << "|   a   |   rotate model left (Y-axis)    |" << std::endl
+              << "|   s   |   rotate model left (Z-axis)    |" << std::endl
+              << "|   d   |   rotate model right (Y-axis)   |" << std::endl
+              << "|   z   |   zoom in                       |" << std::endl
+              << "|   x   |   zoom out                      |" << std::endl
+              << "|   ↑   |   move camera closer            |" << std::endl
+              << "|   ←   |   move camera left              |" << std::endl
+              << "|   ↓   |   move camera away              |" << std::endl
+              << "|   →   |   move camera right             |" << std::endl
+              << "|_______|_________________________________|" << std::endl
+              << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
-    glutInit(&argc, argv);
-    init();
-    glutDisplayFunc(display);
-    glutReshapeFunc(resize);
-    glutKeyboardFunc(key);
-    glutSpecialFunc(specialKeys);
-    glutIdleFunc(idle);
-    glutMainLoop();
+    if (argc >= 2) {
+        if (argv[1] == "-h" || argv[1] == "--help")
+        {
+            printHelp()
+        } else {
+            std::cout << "Unknown arguments detected" << std::endl
+        }
+    } else {
+        glutInit(&argc, argv);
+        init();
+        glutDisplayFunc(display);
+        glutReshapeFunc(resize);
+        glutKeyboardFunc(key);
+        glutSpecialFunc(specialKeys);
+        glutIdleFunc(idle);
+        glutMainLoop();
+    }
+
     return 0;
 }
