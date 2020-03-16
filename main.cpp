888fba9252ca4f9519576ce51a530412b2e14f12
vincentbudianto <vincent.budianto@gmail.com>
2020-03-16 22:47:12 +0700

    fix help menu

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ void printHelp()
 int main(int argc, char *argv[])
 {
     if (argc >= 2) {
-        if (argv[1] == "-h" || argv[1] == "--help")
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
         {
             printHelp();
         } else {
